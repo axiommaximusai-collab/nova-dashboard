f90855978bf8867eb04da7ab8111241f99755956
Axiom Maximus <axiom@studioshade.co>
2026-02-06 22:17:16 -0700

    Fix public folder serving

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'renderer')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // API Routes
 app.use('/api/goals', goalsRoutes);
